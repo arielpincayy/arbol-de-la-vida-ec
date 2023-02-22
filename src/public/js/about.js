35598839194fad6b018af6c3c9428a7a7d75ba18
arielpincay84 <arielpincay84@gmail.com>
2023-02-22 10:30:54 -0500

    V-22-2-23

--- src/public/js/about.js
+++ src/public/js/about.js
@@ -1,11 +1,20 @@
 const story = document.querySelector('.story img');
+const collage = document.querySelector('.collage img');
 
 const shakeImage=(entrada,observador)=>{
     entrada.forEach(e=>{
         if (e.isIntersecting) {
-            console.log('entranding uwu');
-            story.classList.remove('story-img');
             story.classList.add('story-img_toggle');
+            story.classList.remove('story-img');
+        }
+    });
+}
+const shakeCollage=(entrada,observador)=>{
+    entrada.forEach(e=>{
+        if (e.isIntersecting) {
+            console.log('enter');
+            collage.classList.add('collage_img_toggle');
+            collage.classList.remove('collage_img');
         }
     });
 }
@@ -14,4 +23,10 @@ const observer= new IntersectionObserver(shakeImage,{
     rootMargin: "0px",
     threshold: 0.2
 });
+const observer2= new IntersectionObserver(shakeCollage,{
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.2
+});
 observer.observe(story);
+observer2.observe(collage);
