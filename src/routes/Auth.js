18bfb996983e21aaa33a4bbda37eec9de3e1b9c4
arielpincayy <84736532+arielpincayy@users.noreply.github.com>
2023-09-14 14:39:01 -0500

    V.14-09-23

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -24,7 +24,7 @@ app.post('/signUp',(req,res)=>{
     .then((userCredential) => {
       // Signed in
           const accesToken = generateAccessToken(email);
-          res.redirect('/'+home+'?accesToken='+accesToken);
+          res.redirect('/home'+'?accesToken='+accesToken);
     })
     .catch((error) => {
         if (error.message==="Firebase: Error (auth/email-already-in-use).") {
