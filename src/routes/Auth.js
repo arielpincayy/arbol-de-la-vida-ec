54f79eb6774ac0338859fce649e1243f9de2946e
arielpincay84 <arielpincay84@gmail.com>
2023-09-15 13:20:31 -0500

    V.15-09-23

--- src/routes/Auth.js
+++ src/routes/Auth.js
@@ -45,7 +45,7 @@ app.post('/signIn',(req,res)=>{
           // Signed in
           const user = userCredential.user;
           const accesToken = generateAccessToken(email);
-          if (typeof route !== 'string'){route=''}
+          if (typeof route !== 'string'){route= ''}
           res.redirect("/"+route+'?accesToken='+accesToken);
           // ...
         })
